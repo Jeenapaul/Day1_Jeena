f3f4934cbec4f5bc53cea3dd95504d9198f4d319
macstudent <jeenapaul1014@gmail.com>
2019-09-18 19:23:20 -0400

    [Jeena] Comment Added

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -4,10 +4,14 @@ public class FirstDay {
     public static void main(String[] args)
     {
         System.out.println("Jeena");
+        //variable declaration
         int a,b,c;
+        //value Assignment
         a=3;
         b=5;
+        //perform adf
         c=a+b;
+        //display result
         System.out.println("SUM:" +c);
     }
 }
