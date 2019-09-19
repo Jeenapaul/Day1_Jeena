7b0f1cd39e659a1622ccd27ffec2c5fa2c4bc827
macstudent <jeenapaul1014@gmail.com>
2019-09-18 21:00:22 -0400

    JeenA LOOP CREATION

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -1,17 +1,31 @@
 package com.lambton;
 
 public class FirstDay {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Jeena");
         //variable declaration
-        int a,b,c;
+        int a, b, c;
         //value Assignment
-        a=3;
-        b=5;
+        a = 3;
+        b = 5;
         //perform adf
-        c=a+b;
+        c = a + b;
         //display result
-        System.out.println("SUM:" +c);
+        System.out.println("SUM:" + c);
+        int x = Integer.MAX_VALUE;
+        System.out.println("x:" + x);
+        int y = x + 1;
+        System.out.println("y:" + y);
+        System.out.println("Min:" + Integer.MIN_VALUE);
+        StudentJeena j1 = new StudentJeena();
+        j1.setvalues();
+        j1.Display();
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("jeena");
+
+
+        }
     }
 }
--- src/com/lambton/StudentJeena.java
+++ src/com/lambton/StudentJeena.java
@@ -1,6 +1,18 @@
 package com.lambton;
 
 public class StudentJeena {
+
     int studentID;
     String studentName;
+    void setvalues()
+    {
+        studentID=1;
+        studentName ="Jeena";
+    }
+    void Display()
+    {
+        System.out.println("student ID :"+studentID);
+        System.out.println("Student Name:"+studentName);
+
+    }
 }
